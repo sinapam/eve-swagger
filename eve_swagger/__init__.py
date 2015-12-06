7eb386aac2c01bc56a78be28e58abb33b48c3dab
pam <sinapam@proteus-tech.com>
2015-12-06 19:29:15 +0700

    tried to do relative import so that things don't clash

--- eve_swagger/__init__.py
+++ eve_swagger/__init__.py
@@ -7,7 +7,7 @@
     :copyright: (c) 2015 by Nicola Iarocci.
     :license: BSD, see LICENSE for more details.
 """
-from swagger import swagger  # noqa
+from .swagger import swagger  # noqa
 
 SWAGGER = 'SWAGGER'
 
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
+from eve_swagger import __version__
 setup(
   name='eve-swagger',
-  version='0.1',
+  version=__version__,
   packages=['eve_swagger'],
 )
